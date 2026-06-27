7835647e2ac5a179740f2e992d953d41cb5a806b
rocwg <842875195@qq.com>
2026-06-27 22:04:36 +0800

    add: grpc

---
+++ grpc-client/src/main/java/org/springframework/grpc/sample/GrpcClientApplication.java
@@ -0,0 +1,26 @@
+package org.springframework.grpc.sample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.grpc.client.ImportGrpcClients;
+import org.springframework.grpc.sample.proto.HelloRequest;
+import org.springframework.grpc.sample.proto.SimpleGrpc;
+
+@SpringBootApplication
+@ImportGrpcClients
+public class GrpcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcClientApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(SimpleGrpc.SimpleBlockingStub stub) {
+		return args -> {
+			System.out.println(stub.sayHello(HelloRequest.newBuilder().setName("Alien").build()));
+		};
+	}
+
+}
---
+++ grpc-server/src/main/java/org/springframework/grpc/sample/GrpcServerApplication.java
@@ -0,0 +1,30 @@
+package org.springframework.grpc.sample;
+
+import io.grpc.Metadata;
+import io.grpc.Status;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.grpc.server.exception.GrpcExceptionHandler;
+
+@SpringBootApplication
+public class GrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+
+    @Bean
+    public GrpcExceptionHandler globalInterceptor() {
+        return exception -> {
+            if (exception instanceof IllegalArgumentException) {
+                Metadata metadata = new Metadata();
+                metadata.put(Metadata.Key.of("error-code", Metadata.ASCII_STRING_MARSHALLER), "INVALID_ARGUMENT");
+                return Status.INVALID_ARGUMENT.withDescription(exception.getMessage())
+                    .asException(metadata);
+            }
+            return null;
+        };
+    }
+
+}
